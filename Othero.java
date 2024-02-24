237c9f4712b131f03419cf1fde39c4c6fb4f7163
hit0sh1 <h.miyagawa365@gmail.com>
2024-02-24 22:23:50 +0900

    ✨ オセロゲーム

---
+++ Othero.java
@@ -0,0 +1,14 @@
+public class Othero {
+  
+}
+
+// スキャナーを閉じる
+scanner.close();
+
+RockPaperScissors.javaファイルは、現在のディレクトリ内にあります();
+
+// オセロゲームの作成
+public class OthelloGame {
+    // ゲームのロジックを実装する
+    // メソッドやフィールドを追加して、オセロゲームを完成させる
+}
